16d1a660fd09345122f5e32625d7b011302ad8c3
rishabhsingh507 <rishabhsingh507@gmail.com>
2021-05-30 18:05:18 +0530

    feat: Added header

---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,18 @@
+import classes from "./Header.module.css";
+import { Fragment } from "react";
+
+const Header = props => {
+    return (
+        <Fragment>
+            <header className={classes.header}>
+                <h1>Chocolate ROom</h1>
+                <button>Cart</button>
+            </header>
+            <div className={classes['main-image']}>
+                <img src="https://images.yourstory.com/cs/2/f02aced0d86311e98e0865c1f0fe59a2/chocolate-industry-1619632472826.png" alt="Chocolates" />
+            </div>
+        </Fragment>
+    )
+}
+
+export default Header;
